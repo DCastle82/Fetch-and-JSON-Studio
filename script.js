2aba620e06ec66ad80cab3d63523a0c2c91fc6fc
DCastle82 <99931328+DCastle82@users.noreply.github.com>
2022-03-10 19:52:43 -0600

    First Commit

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// TODO: add code here
+window.addEventListener('load', function(){
+    let astroInfo = [];
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(json) {
+           console.log(json);
+        });
+     });
+     const destination = document.getElementById("destination");
+     let htmlOut = "";
+       for(let astronauts of json){
+           htmlOut += `<h1> ${astronauts.name}`
+       }
+       container.innerHTML = htmlOut;
+
+});
